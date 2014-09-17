044e68544261150794451be3093e65c688b44e19
Zaixing Mao <zaixing.mao@cern.ch>
2014-09-17 11:27:39 +0200

    minor changes

--- changeFiles.sh
+++ changeFiles.sh
@@ -1,5 +1,5 @@
-mv mvaPFMET_cff.py ../JetMETCorrections/METPUSubtraction/python
-mv mvaPFMET_leptons_cff.py ../JetMETCorrections/METPUSubtraction/python
-mv mvaPFMET_leptons_data_cff.py ../JetMETCorrections/METPUSubtraction/python
-mv mvaPFMET_leptons_PAT_cfi.py ../JetMETCorrections/METPUSubtraction/python
-mv mvaPFMET_leptons_PAT_old_cfi.py ../JetMETCorrections/METPUSubtraction/python
+cp mvaPFMET_cff.py ../JetMETCorrections/METPUSubtraction/python/
+cp mvaPFMET_leptons_cff.py ../JetMETCorrections/METPUSubtraction/python/
+cp mvaPFMET_leptons_data_cff.py ../JetMETCorrections/METPUSubtraction/python/
+cp mvaPFMET_leptons_PAT_cfi.py ../JetMETCorrections/METPUSubtraction/python/
+cp mvaPFMET_leptons_PAT_old_cfi.py ../JetMETCorrections/METPUSubtraction/python/

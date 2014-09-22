df16a3b10ce73f3639c69c85308504b05df283a1
Edward Laird <elaird@gmail.com>
2014-09-22 12:02:29 -0500

    copy additional file

--- changeFiles.sh
+++ changeFiles.sh
@@ -3,3 +3,4 @@ cp mvaPFMET_leptons_cff.py ../JetMETCorrections/METPUSubtraction/python/
 cp mvaPFMET_leptons_data_cff.py ../JetMETCorrections/METPUSubtraction/python/
 cp mvaPFMET_leptons_PAT_cfi.py ../JetMETCorrections/METPUSubtraction/python/
 cp mvaPFMET_leptons_PAT_old_cfi.py ../JetMETCorrections/METPUSubtraction/python/
+cp PFMETAlgorithmMVA.cc ../JetMETCorrections/METPUSubtraction/src/

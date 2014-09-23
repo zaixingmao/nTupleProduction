e782bfd1ce5d954d378655270e2410aabc9da84e
Zaixing <zaixing.mao@cern.ch>
2014-09-23 10:48:23 -0500

    changes for IC-MVAMet

---
+++ classes.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include <map>
+#include <utility>
+#include <string>
+
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/BTauReco/interface/SecondaryVertexTagInfo.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+namespace { struct dictionary {
+  edm::Wrapper<std::vector<unsigned long> > dummy45;
+
+};
+}

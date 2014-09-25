6750c2c5969e7a97715dcc2c763a3b5d5c1204e2
Zaixing <zaixing.mao@cern.ch>
2014-09-25 08:01:08 -0500

    add IC-MVA files

--- changeFiles.sh
+++ changeFiles.sh
@@ -4,3 +4,9 @@ cp mvaPFMET_leptons_data_cff.py ../JetMETCorrections/METPUSubtraction/python/
 cp mvaPFMET_leptons_PAT_cfi.py ../JetMETCorrections/METPUSubtraction/python/
 cp mvaPFMET_leptons_PAT_old_cfi.py ../JetMETCorrections/METPUSubtraction/python/
 cp PFMETAlgorithmMVA.cc ../JetMETCorrections/METPUSubtraction/src/
+
+cp classes.h ../JetMETCorrections/METPUSubtraction/src/
+cp classes_def.xml ../JetMETCorrections/METPUSubtraction/src/
+cp MVAMETPairProducer.cc ../JetMETCorrections/METPUSubtraction/plugins/
+cp MVAMETPairProducer.hh ../JetMETCorrections/METPUSubtraction/plugins/
+cp BuildFile.xml ../JetMETCorrections/

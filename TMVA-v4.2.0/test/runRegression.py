a3cb964486a70af385d519a117310eb7e49ae405
Zaixing <zaixing.mao@cern.ch>
2014-11-05 08:35:04 -0600

    update BDT setting for more mass points

--- TMVA-v4.2.0/test/runRegression.py
+++ TMVA-v4.2.0/test/runRegression.py
@@ -2,9 +2,18 @@
 
 import os as os
 
-fileList = ['H2hh260',
+fileList = [ # 'H2hh260',
 #            'H2hh300',
 #            'H2hh350',
+           'H2hh270',
+           'H2hh280',
+           'H2hh290',
+           'H2hh310',
+           'H2hh320',
+           'H2hh330',
+           'H2hh340',
+           'H2hh500',
+           'H2hh700',
 #            'tt_eff',
 #            'tt_semi_eff',
 #            'ZZ_eff',
@@ -23,7 +32,7 @@ fileList = ['H2hh260',
 #               'TTJets_MSDecays',
 
             ]
-location = '/scratch/zmao/relaxed_regression4/'
+location = '/scratch/zmao/triggerMatch/'
 
 for iFile in fileList:
     rootCommand = "root -l -q  BJetRegressionApplication.C\(\\\"BDTG\\\",\\\"%s_all.root\\\",\\\"%s\\\"\)" %(iFile, location)

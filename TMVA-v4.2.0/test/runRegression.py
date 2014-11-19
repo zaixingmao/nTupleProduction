98393527c7e371f4709adbada6a1ec2288beeb5f
Zaixing <zaixing.mao@cern.ch>
2014-11-19 03:32:54 -0600

    config changes

--- TMVA-v4.2.0/test/runRegression.py
+++ TMVA-v4.2.0/test/runRegression.py
@@ -2,29 +2,29 @@
 
 import os as os
 
-fileList = [ # 'H2hh260',
-#            'H2hh300',
-#            'H2hh350',
-#            'H2hh270',
-#            'H2hh280',
-#            'H2hh290',
-#            'H2hh310',
-#            'H2hh320',
-#            'H2hh330',
-#            'H2hh340',
-#            'H2hh500',
-#            'H2hh700',
-#            'tt_eff',
-#            'tt_semi_eff',
-#            'ZZ_eff',
+fileList = ['H2hh260',
+           'H2hh300',
+           'H2hh350',
+           'H2hh270',
+           'H2hh280',
+           'H2hh290',
+           'H2hh310',
+           'H2hh320',
+           'H2hh330',
+           'H2hh340',
+           'H2hh500',
+           'H2hh700',
+           'tt',
+           'tt_semi',
+           'ZZ',
            'DY1JetsToLL',
            'DY2JetsToLL',
            'DY3JetsToLL',
            'W1JetsToLNu',
            'W2JetsToLNu',
            'W3JetsToLNu',
-#            'WZJetsTo2L2Q_eff',
-#            'dataTotal',
+           'WZJetsTo2L2Q',
+           'dataTotal',
 #             'QCD_Pt-50to80',
 #               'VBF_HToTauTau',
 #               'GluGluToHToTauTau',
@@ -32,7 +32,7 @@ fileList = [ # 'H2hh260',
 #               'TTJets_MSDecays',
 
             ]
-location = '/scratch/zmao/triggerMatch/'
+location = '/scratch/zmao/v3_2/'
 
 for iFile in fileList:
     rootCommand = "root -l -q  BJetRegressionApplication.C\(\\\"BDTG\\\",\\\"%s_all.root\\\",\\\"%s\\\"\)" %(iFile, location)
